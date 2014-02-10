0b2baa2ff73694f72719f39708273de14b39cb77
kristenmills <kristen@kristen-mills.com>
2014-02-10 14:47:54 -0500

    Added checkout model

---
+++ Alexandria/Models/CheckOut.h
@@ -0,0 +1,13 @@
+//
+//  CheckOut.h
+//  Alexandria
+//
+//  Created by Kristen Mills on 2/10/14.
+//  Copyright (c) 2014 Society of Software Engineers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CheckOut : NSObject
+
+@end
---
+++ Alexandria/Models/CheckOut.m
@@ -0,0 +1,13 @@
+//
+//  CheckOut.m
+//  Alexandria
+//
+//  Created by Kristen Mills on 2/10/14.
+//  Copyright (c) 2014 Society of Software Engineers. All rights reserved.
+//
+
+#import "CheckOut.h"
+
+@implementation CheckOut
+
+@end
