6bef2601bd3f046b8bd537454a3f96d629d376a3
kristenmills <kristen@kristen-mills.com>
2014-02-11 20:56:10 -0500

    Added PutAwayViewController

---
+++ Alexandria/Controllers/PutAwayViewController.m
@@ -0,0 +1,38 @@
+//
+//  PutAwayViewController.m
+//  Alexandria
+//
+//  Created by Kristen Mills on 2/11/14.
+//  Copyright (c) 2014 Society of Software Engineers. All rights reserved.
+//
+
+#import "PutAwayViewController.h"
+
+@interface PutAwayViewController ()
+
+@end
+
+@implementation PutAwayViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
