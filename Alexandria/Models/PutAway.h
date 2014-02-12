11e248f49ca9a8d1693ad4ea4b0d266ea7f79272
kristenmills <kristen@kristen-mills.com>
2014-02-11 21:37:57 -0500

    Added PutAway model

---
+++ Alexandria/Models/PutAway.h
@@ -0,0 +1,13 @@
+//
+//  PutAway.h
+//  Alexandria
+//
+//  Created by Kristen Mills on 2/11/14.
+//  Copyright (c) 2014 Society of Software Engineers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PutAway : NSObject
+
+@end
