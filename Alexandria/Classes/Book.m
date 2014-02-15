6ef2a5ce3ba82b8cb939df3adfe5dd855145b90e
kristenmills <kristen@kristen-mills.com>
2014-02-15 10:27:17 -0500

    Added Image downloader and book classes

---
+++ Alexandria/Classes/Book.m
@@ -0,0 +1,13 @@
+//
+//  Book.m
+//  Alexandria
+//
+//  Created by Kristen Mills on 2/15/14.
+//  Copyright (c) 2014 Society of Software Engineers. All rights reserved.
+//
+
+#import "Book.h"
+
+@implementation Book
+
+@end
---
+++ Alexandria/ImageDownloader.m
@@ -0,0 +1,13 @@
+//
+//  IconDownloader.m
+//  Alexandria
+//
+//  Created by Kristen Mills on 2/15/14.
+//  Copyright (c) 2014 Society of Software Engineers. All rights reserved.
+//
+
+#import "IconDownloader.h"
+
+@implementation IconDownloader
+
+@end
